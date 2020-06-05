94e6d649dfa0a08d8313af385dd9b44405d80922
김수연 <onemask@naver.com>
2020-06-06 00:47:22 +0900

    create view package

--- kakao/app/src/main/java/com/kakao/kakaosearch/view/MainActivity.kt
+++ kakao/app/src/main/java/com/kakao/kakaosearch/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kakao.kakaosearch
+package com.kakao.kakaosearch.view
 
 import android.app.SearchManager
 import android.content.Context
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.widget.SearchView
+import com.kakao.kakaosearch.R
 
 class MainActivity : AppCompatActivity() {
 
