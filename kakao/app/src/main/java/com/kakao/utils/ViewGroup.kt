c3d59c6ff23b9d2a6f51da0e8ad0a2c9a727fee8
김수연 <onemask@naver.com>
2020-06-07 22:32:29 +0900

    Add Extension

---
+++ kakao/app/src/main/java/com/kakao/utils/ViewGroup.kt
@@ -0,0 +1,9 @@
+package com.kakao.utils
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+
+
+fun ViewGroup.inflate(@LayoutRes resource: Int, attachRoot: Boolean = false): View = LayoutInflater.from(context).inflate(resource, this, attachRoot)
