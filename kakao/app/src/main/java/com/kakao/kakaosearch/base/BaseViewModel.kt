f73fe082eaa9119f0bb2c8e092a4ebf079553891
김수연 <onemask@naver.com>
2020-06-07 22:29:46 +0900

    Add BaseViewmodel

---
+++ kakao/app/src/main/java/com/kakao/kakaosearch/base/BaseViewModel.kt
@@ -0,0 +1,15 @@
+package com.kakao.kakaosearch.base
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+
+    protected val disposable = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+
+}
