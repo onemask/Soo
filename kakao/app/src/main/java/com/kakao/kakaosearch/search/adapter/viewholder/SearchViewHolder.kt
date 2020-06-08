50c17689ebee3caf00b301adce099373e99259cd
soo <onemask@stunitas.com>
2020-06-08 15:23:38 +0900

    SearchViewHolder Binding 적용

--- kakao/app/src/main/java/com/kakao/kakaosearch/search/adapter/viewholder/SearchViewHolder.kt
+++ kakao/app/src/main/java/com/kakao/kakaosearch/search/adapter/viewholder/SearchViewHolder.kt
@@ -1,22 +1,11 @@
 package com.kakao.kakaosearch.search.adapter.viewholder
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.kakao.kakaosearch.R
+import com.kakao.kakaosearch.databinding.ItemKakaoImageBinding
 import com.kakao.kakaosearch.repository.model.Document
-import kotlinx.android.synthetic.main.item_kakao_image.view.*
 
-class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class SearchViewHolder(private val binding: ItemKakaoImageBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(data: Document) {
-        with(itemView) {
-            data.let {
-                //img
-                Glide.with(this.context)
-                    .load(it.thumbnail_url)
-                    .placeholder(R.mipmap.ic_launcher_round)
-                    .into(this.image)
-            }
-        }
+        binding.document = data
     }
 }
