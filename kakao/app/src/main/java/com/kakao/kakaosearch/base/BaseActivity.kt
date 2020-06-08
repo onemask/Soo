04f5260ee8957ffa54791a22c6f1d70a5a920e6e
김수연 <onemask@naver.com>
2020-06-08 23:56:18 +0900

    BasesActivity sestupViewModel 추가

--- kakao/app/src/main/java/com/kakao/kakaosearch/base/BaseActivity.kt
+++ kakao/app/src/main/java/com/kakao/kakaosearch/base/BaseActivity.kt
@@ -7,16 +7,20 @@ import androidx.databinding.ViewDataBinding
 import dagger.android.AndroidInjection
 import dagger.android.support.DaggerAppCompatActivity
 
-abstract class BaseActivity<B : ViewDataBinding, VM : BaseViewModel>
-    (@LayoutRes private val layoutRes: Int) : DaggerAppCompatActivity() {
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int
+) : DaggerAppCompatActivity() {
 
     protected lateinit var binding: B
-    abstract val viewModel: VM
+    abstract val viewModel: BaseViewModel
+
+    abstract fun setupViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutRes)
         binding.lifecycleOwner = this
+        setupViewModel()
     }
 }
