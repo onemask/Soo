91271f980c3d3b7bf58063acca4cee3e6e1d664a
soo <onemask@stunitas.com>
2020-06-08 17:55:55 +0900

    BindingAdapter 추가

--- kakao/app/src/main/java/com/kakao/utils/BindingAdapter.kt
+++ kakao/app/src/main/java/com/kakao/utils/BindingAdapter.kt
@@ -4,14 +4,14 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.kakao.kakaosearch.R
-import kotlinx.android.synthetic.main.item_kakao_image.view.*
 
 object BindingAdapter {
     @JvmStatic
     @BindingAdapter("srcCompat")
     fun srcCompat(view: ImageView, url: String) {
-        Glide.with(view.context).load(url).thumbnail(0.1f)
+        Glide.with(view.context)
+            .load(url)
             .placeholder(R.mipmap.ic_launcher_round)
-            .into(view.image)
+            .into(view)
     }
 }
