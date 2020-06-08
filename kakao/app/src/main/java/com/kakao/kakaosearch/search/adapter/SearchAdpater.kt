1c649fb9ecdd0d8b38d9e4f9a4acf2f170139472
soo <onemask@stunitas.com>
2020-06-08 15:24:05 +0900

    SearchAdapter DataBinding 적용

--- kakao/app/src/main/java/com/kakao/kakaosearch/search/adapter/SearchAdpater.kt
+++ kakao/app/src/main/java/com/kakao/kakaosearch/search/adapter/SearchAdpater.kt
@@ -1,9 +1,12 @@
 package com.kakao.kakaosearch.search.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.kakao.kakaosearch.R
+import com.kakao.kakaosearch.databinding.ItemKakaoImageBinding
 import com.kakao.kakaosearch.repository.model.Document
 import com.kakao.kakaosearch.search.adapter.viewholder.SearchViewHolder
 import com.kakao.utils.inflate
@@ -15,8 +18,15 @@ class SearchAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val filterDataSet = ArrayList<Document>()
     private val tempList = ArrayList<Document>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-        SearchViewHolder(parent.inflate(R.layout.item_kakao_image))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val item = DataBindingUtil.inflate<ItemKakaoImageBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_kakao_image,
+            parent,
+            false
+        )
+        return SearchViewHolder(item)
+    }
 
     override fun getItemCount(): Int = dataSet.size
 
