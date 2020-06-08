cdd0899478cfed1d38b1b061eef20b9919420548
김수연 <onemask@naver.com>
2020-06-09 00:01:25 +0900

    MainActivity 로직 수정

--- kakao/app/src/main/java/com/kakao/kakaosearch/search/MainActivity.kt
+++ kakao/app/src/main/java/com/kakao/kakaosearch/search/MainActivity.kt
@@ -4,27 +4,26 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.SearchView
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kakao.kakaosearch.R
 import com.kakao.kakaosearch.base.BaseActivity
 import com.kakao.kakaosearch.databinding.ActivityMainBinding
-import com.kakao.kakaosearch.repository.KaKaoRepositoryImpl
 import com.kakao.kakaosearch.search.adapter.SearchAdapter
 import com.kakao.kakaosearch.search.vm.MainViewModel
-import com.kakao.kakaosearch.search.vm.MainViewModelFactory
-import com.kakao.utils.EndlessRecyclerOnScrollListener
 import kotlinx.android.synthetic.main.activity_main.*
-import timber.log.Timber
 import javax.inject.Inject
 
-class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
-
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    layoutRes = R.layout.activity_main
+) {
     @Inject
-    lateinit var kaKaoRepository: KaKaoRepositoryImpl
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val lists = mutableListOf("all")
+    private val adapter = SearchAdapter()
     private val spinnerAdapter by lazy {
         ArrayAdapter<String>(
             this,
@@ -32,92 +31,67 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
             lists
         )
     }
-    private val adapter by lazy { SearchAdapter() }
-    private val viewModelFactory by lazy { MainViewModelFactory(kaKaoRepository) }
-    private var searchKeyword: String = ""
-    private val lists = mutableListOf("all")
+    override val viewModel: MainViewModel by lazy {
+        ViewModelProvider(
+            this@MainActivity,
+            viewModelFactory
+        )[MainViewModel::class.java]
+    }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setupAdapter()
         setBind()
         setupListener()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun setupViewModel() {
         binding.vm = viewModel
     }
 
-    override val viewModel by lazy {
-        ViewModelProviders.of(
-            this@MainActivity,
-            viewModelFactory
-        )[MainViewModel::class.java]
-
-    }
-
     private fun setupAdapter() {
-        rv.adapter = adapter
-        sp_filter.adapter = spinnerAdapter
+        binding.rv.adapter = adapter
+        binding.spFilter.adapter = spinnerAdapter
     }
 
     private fun setBind() {
-        viewModel.searchResult.observe(this, Observer {
-            if (viewModel.currentPage == 1L)
+        viewModel.run {
+            val lifecycleOwner = this@MainActivity
+            searchResult.observe(lifecycleOwner, Observer {
                 adapter.setData(it)
-            else
-                adapter.addData(it)
-        })
-
-        viewModel.filter.observe(this, Observer {
-            lists.apply {
-                clear()
-                add("all")
-                addAll(it)
-            }
-            spinnerAdapter.notifyDataSetChanged()
-        })
+            })
+            filterList.observe(lifecycleOwner, Observer {
+                lists.run {
+                    clear()
+                    add("all")
+                    addAll(it)
+                }
+                spinnerAdapter.notifyDataSetChanged()
+            })
+        }
     }
 
     private fun setupListener() {
-        sv.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
-            androidx.appcompat.widget.SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                query?.run {
-                    if (this.isNotEmpty()) {
-                        searchKeyword = this
-                        viewModel.getSearch(searchKeyword = searchKeyword)
-                        sp_filter.setSelection(0)
-                    }
-                }
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-        })
-
         sp_filter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onNothingSelected(parent: AdapterView<*>?) = Unit
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
                 position: Int,
                 id: Long
             ) {
-                adapter.filterData(lists[position])
+                viewModel.setFilter(lists[position])
             }
         }
 
-        rv.addOnScrollListener(object : EndlessRecyclerOnScrollListener(rv.layoutManager as LinearLayoutManager) {
-            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?) {
-                Timber.d("page $page")
-                viewModel.currentPage = page.toLong()
-                if (viewModel.currentPage < viewModel.totalPage) {
-                    viewModel.getSearch(searchKeyword)
-                    viewModel.currentPage++
+        rv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) return
+                adapter.itemCount.let {
+                    if ((rv.layoutManager as LinearLayoutManager).findLastVisibleItemPosition() + 2 >= it) {
+                        viewModel.loadMore()
+                    }
                 }
             }
         })
