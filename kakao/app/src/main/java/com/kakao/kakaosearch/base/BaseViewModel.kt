540cafc907fbb6d7cfc93f222db4cc94a5dc0304
soo <onemask@stunitas.com>
2020-06-08 15:22:22 +0900

    BaseViewModel에 loadingState 추가

--- kakao/app/src/main/java/com/kakao/kakaosearch/base/BaseViewModel.kt
+++ kakao/app/src/main/java/com/kakao/kakaosearch/base/BaseViewModel.kt
@@ -1,11 +1,17 @@
 package com.kakao.kakaosearch.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseViewModel : ViewModel() {
 
     protected val disposable = CompositeDisposable()
+    protected var _loadingState = MutableLiveData<Boolean>()
+    val loadingState: LiveData<Boolean>
+        get() = _loadingState
+
 
     override fun onCleared() {
         super.onCleared()
